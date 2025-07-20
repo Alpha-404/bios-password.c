1a4591a30027f5d540c375f464091c240685f43d
axjp <63340001+Alpha-404@users.noreply.github.com>
2025-07-20 15:44:40 +0100

    real hpami commit, forgot a few files in prev commit

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
-CFLAGS = -O2 -Wall -DENABLE_SONY -DENABLE_HPMINI -DENABLE_PHOENIX # add -O2 for optim
+CFLAGS = -O2 -Wall -DENABLE_SONY -DENABLE_HPMINI -DENABLE_PHOENIX -DENABLE_HPAMI# add -O2 for optim
 LDFLAGS = -lm
-SRCS = main.c utils/solver.c utils/utils.c keygens/sony.c keygens/hpmini.c keygens/phoenix.c
+SRCS = main.c utils/solver.c utils/utils.c utils/cryptoUtils.c keygens/sony.c keygens/hpmini.c keygens/phoenix.c keygens/hpami.c
 OBJS = $(SRCS:.c=.o)
 TARGET = keygen
 
--- main.c
+++ main.c
@@ -1,21 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "keygens/sony.h"
+#include "keygens/hpmini.h"
+#include "keygens/phoenix.h"
+#include "keygens/hpami.h"
+
+#include "utils/solver.h"
+
+#define MAX_SOLVERS 16
 
 typedef char* (*KeygenFunc)(const char*);
 
+/*
 struct {
     const char *vendor;
     KeygenFunc func;
 } keygens[] = {
     { "sony", sonySolver },
+    {"hpmini", hpminiSolver},
     { NULL, NULL }
 };
+*/
+
+void list_solvers(const Solver solvers[]) {
+    for (int i = 0; solvers[i].name != NULL; i++) {
+        printf("%s: %s\n", solvers[i].name, solvers[i].description);
+    }
+}
 
 int main(int argc, char *argv[]) {
+    Solver solvers[MAX_SOLVERS];
+    int count = 0;
+    
+    #ifdef ENABLE_SONY
+        solvers[count++] = makeSolver("sony", "Sony 7-digit numeric keygen", "^[0-9]{7}$", sonyKeygen);
+    #endif
+
+    #ifdef ENABLE_HPMINI
+        solvers[count++] = makeSolver("hpmini", "HP Mini 10-character alphanumeric keygen", "^[0-9A-Z]{10}$", hpminiKeygen);
+    #endif
+
+    #ifdef ENABLE_PHOENIX
+        PhoenixSolver phoenix = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenix",
+            .description = "Generic Phoenix",
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenix.base;
+
+        PhoenixSolver phoenixHPCompaq = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenixHP",
+            .description = "HP/Compaq Phoenix BIOS",
+            .salt = 17232,
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenixHPCompaq.base;
+
+        PhoenixSolver phoenixFSI = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenixFSI",
+            .description = "Fujitsu-Siemens Phoenix",
+            .salt = 65,
+            .dictionary = digitsOnly,
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenixFSI.base;
+
+        PhoenixSolver phoenixFSIModelL = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenixFSIModelL",
+            .description = "Fujitsu-Siemens (model L) Phoenix",
+            .shift = 1,
+            .salt = 'L',
+            .dictionary = digitsOnly,
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenixFSIModelL.base;
+
+        PhoenixSolver phoenixFSIModelP = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenixFSIModelP",
+            .description = "Fujitsu-Siemens (model P) Phoenix",
+            .shift = 1,
+            .salt = 'P',
+            .dictionary = digitsOnly,
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenixFSIModelP.base;
+
+        PhoenixSolver phoenixFSIModelS = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenixFSIModelS",
+            .description = "Fujitsu-Siemens (model S) Phoenix",
+            .shift = 1,
+            .salt = 'S',
+            .dictionary = digitsOnly,
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenixFSIModelS.base;
+
+        PhoenixSolver phoenixFSIModelX = makePhoenixSolver(&(PhoenixBios){
+            .name = "phoenixFSIModelX",
+            .description = "Fujitsu-Siemens (model S) Phoenix",
+            .shift = 1,
+            .salt = 'X',
+            .dictionary = digitsOnly,
+            .pattern = ".*"
+        });
+        solvers[count++] = phoenixFSIModelX.base;
+    #endif
+
+    #ifdef ENABLE_HPAMI
+        solvers[count++] = makeSolver("hpami", "HP AMI", "^[0-9ABCDEF]{8}$", hpAmiKeygen);
+    #endif
+
+    solvers[count++] = (Solver){ NULL, NULL, NULL, NULL };
+
     if (argc != 3) {
         fprintf(stderr, "Usage: %s VENDOR SERIAL\n", argv[0]);
+        printf("Keygens: \n");
+        list_solvers(solvers);
         return 1;
     }
 
@@ -23,9 +125,9 @@ int main(int argc, char *argv[]) {
     const char *serial = argv[2];
     char *code = NULL;
 
-    for (int i = 0; keygens[i].vendor; ++i) {
-        if (strcmp(keygens[i].vendor, vendor) == 0) {
-            code = keygens[i].func(serial);
+    for (int i = 0; solvers[i].name; ++i) {
+        if (strcmp(solvers[i].name, vendor) == 0) {
+            code = runSolver(solvers[i], serial);
             break;
         }
     }
@@ -35,7 +137,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("Unlock code: %s\n", code);
+    printf("Unlock code(s): %s\n", code);
     free(code);
     return 0;
 }
